7aeadd18ede4fc4f5ccd77ba3b20386fa2f95c48
John Ossai <johnossai20@gmail.com>
2024-01-09 09:13:04 -0800

    checj error

--- src/data.js
+++ src/data.js
@@ -1,20 +1,20 @@
 export const articleData = [
   {
     id: 1,
-    title: "Revitalize Your Mornings: A 7-Day Wellness Challenge",
-    desc: "Embark on a transformative journey with our week-long wellness challenge. Dive into revitalizing morning routines, nourishing breakfast ideas.....",
+    title: "Navigating the World of Superfoods: A Comprehensive Guide",
+    desc: "Explore the nutritional wonders of superfoods! Our guide takes you through the benefits of these powerhouse ingredients, providing insights....",
     img: require("../public/ArticleImageOne.png"),
   },
   {
     id: 1,
-    title: "Navigating the World of Superfoods: A Comprehensive Guide",
-    desc: "Explore the nutritional wonders of superfoods! Our guide takes you through the benefits of these powerhouse ingredients, providing insights, recipes....",
+    title: "Sculpt and Tone: Effective Home Workouts for Busy Schedules",
+    desc: "Discover the secret to achieving a sculpted body without leaving your home. Our collection of efficient home workouts caters to busy schedules.....",
     img: require("../public/ArticleImageTwo.png"),
   },
   {
     id: 1,
-    title: "Sculpt & Tone: Effective Home Workouts 4 Schedules",
-    desc: "Embark on a transformative journey with our week-long wellness challenge. Dive into revitalizing morning routines, nourishing breakfast ideas.....",
+    title: "Balancing Act: The Art of Juggling Fitness and Work",
+    desc: "Struggling to maintain a balance between work and fitness? Dive into our article on practical tips and strategies to integrate exercise seamlessly.....",
     img: require("../public/ArticleImageThree.png"),
   },
 ];
@@ -27,12 +27,12 @@ export const tipData = [
   },
   {
     id: 2,
-    title: "Cardiovascular Boost",
-    text: "Stay energized throughout the day by incorporating 30 minutes of brisk walking, jogging, or cycling into your routine. Cardio exercises not only enhance heart health but also contribute to overall well-being.",
+    title: "Strength Building at Home",
+    text: "No gym? No problem! Utilize your body weight with exercises like squats, push-ups, and planks to build strength at home. These simple yet effective workouts can be done anytime, anywhere.",
   },
   {
     id: 3,
-    title: "Cardiovascular Boost",
-    text: "Stay energized throughout the day by incorporating 30 minutes of brisk walking, jogging, or cycling into your routine. Cardio exercises not only enhance heart health but also contribute to overall well-being.",
+    title: "Consistency is Key",
+    text: "Consistency trumps intensity. Rather than sporadic intense workouts, focus on establishing a regular exercise routine. Small, sustainable efforts lead to long-term fitness success.",
   },
 ];
